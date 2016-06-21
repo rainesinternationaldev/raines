2fce077aac73b683b108b4410cda019423033e9e
Robin Kim <rokim8@gmail.com>
2016-06-20 21:00:46 -0400

    new nav, new font, homepage

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,14 +1,39 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import splashImage from '../assets/city.png'
 import classes from './HomeView.scss'
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
+  <div className={classes.home}>
+    <div className="col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 col-xs-12">
+    	<div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      	<img
+    	  	className={classes.splashImage}
+      		src={splashImage}/>
+      	<hr/>
+      </div>
+      <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      	<div className={classes.center}>
+      		<h3>Well-Established. Well-Connected. Well-Informed.</h3>
+      	</div>
+      	<hr/>
+      </div>
+      <div className={`"col-lg-12 col-md-12 col-sm-12 col-xs-12" ${classes.signup}`}>
+        <div className={classes.inner}>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <input/>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <input/>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <input/>
+          </div>
+          <div className="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+            <button>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
