cdbf2cb4a210b16c444d4a9214b9ffc309ecb7ca
Robin Kim <rokim8@gmail.com>
2016-07-16 17:54:17 -0400

    complete view more component button centering

---
+++ src/routes/IdeasAndInsights/ViewMore.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './IdeasAndInsights.scss';
+
+export const ViewMore = ({
+  viewMore
+}) => (
+  <div className={`${classes.viewMore} col-lg-12 col-md-12 col-sm-12 col-xs-12`}>
+    <div className={classes.border}>
+    </div>
+    <div className={classes.buttonDiv}>
+      <button onClick={viewMore} className={`${classes.viewMoreButton}`}>View More</button>
+    </div>
+    <div className={classes.border}>
+    </div>
+  </div>
+);
--- src/routes/IdeasAndInsights/index.js
+++ src/routes/IdeasAndInsights/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {ViewMore} from './ViewMore';
 import classes from './IdeasAndInsights.scss';
 
 export default class IdeasAndInsights extends React.Component {
+  constructor(props) {
+    super(props);
+    this.displayMoreArticles = () => {
+      console.log('fetching more articles')
+    }
+  }
+
   render() {
     return (
       <div className={`${classes.ideasAndInsights} col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 col-xs-12`}>
@@ -40,23 +48,58 @@ export default class IdeasAndInsights extends React.Component {
             </div>
             <hr/>
           </div>
-          <div className={classes.primaryArticles}>
+          <div className={`${classes.primaryArticles} col-lg-12 col-md-12 col-sm-12 col-xs-12`}>
             <div className={`${classes.latestArticle} col-lg-6 col-md-6 col-sm-12 col-xs-12`}>
               <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
               <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
               <p className={classes.articleSummary}><span className={classes.date}>April 2016</span> - The forces that have driven exceptional investment returns over the past 30 years are weakening, and even reversing. It may be time for investors to lower their expectations...</p>
             </div>
             <div className={`${classes.nextArticles} col-lg-6 col-md-6 col-sm-12 col-xs-12`}>
-              <div className={`${classes.nextArticle} col-lg-6 col-md-6 col-sm-12 col-xs-12`}>
+              <div className={`${classes.nextArticle} col-lg-6 col-md-6 col-sm-6 col-xs-12`}>
                 <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
                 <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
                 <p className={classes.articleSummary}><span className={classes.date}>April 2016</span> - The forces that have driven exceptional investment returns over the past 30 years are weakening, and even reversing. It may be time for investors to lower their expectations...</p>
               </div>
-              <div className={`${classes.nextArticle} col-lg-6 col-md-6 col-sm-12 col-xs-12`}>
+              <div className={`${classes.nextArticle} col-lg-6 col-md-6 col-sm-6 col-xs-12`}>
                 <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
                 <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
                 <p className={classes.articleSummary}><span className={classes.date}>April 2016</span> - The forces that have driven exceptional investment returns over the past 30 years are weakening, and even reversing. It may be time for investors to lower their expectations...</p>
               </div>
+            </div>
+          </div>
+          <ViewMore viewMore={this.displayMoreArticles}/>
+          <div className={`${classes.nextArticle} col-lg-12 col-md-12 col-sm-12 col-xs-12`}>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
+            </div>
+            <div className={`${classes.nextArticle} col-lg-3 col-md-3 col-sm-6 col-xs-12`}>
+              <img src="https://images.unsplash.com/photo-1414496213569-23220f1033cd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=4799ea6a2039b3422d8cb83ea40eed73"/>
+              <h5 className={classes.articleTitle}>Exploding digital flows in a deeply connected world</h5>
             </div>
           </div>
         </div>
