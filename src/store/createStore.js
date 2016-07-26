4ad571b050883461cd9cf7f7ac798a0522b038e2
Robin Kim <rokim8@gmail.com>
2016-07-26 18:37:56 -0400

    implement redux logger

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,13 +1,17 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
+import createLogger from 'redux-logger';
 import thunk from 'redux-thunk'
 import makeRootReducer from './reducers'
 
 export default (initialState = {}, history) => {
+
+  const logger = createLogger({ predicate });
+
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk, routerMiddleware(history)]
+  const middleware = [thunk, logger, routerMiddleware(history)]
 
   // ======================================================
   // Store Enhancers
@@ -41,4 +45,20 @@ export default (initialState = {}, history) => {
   }
 
   return store
+}
+
+/**
+ * Disable logging for specific actions,
+ * and disable ALL logging in production.
+ */
+function predicate(getState, action) {
+	const actionsToIgnore = [];
+	const omitLogger 			= actionsToIgnore.reduce((prev, curr) => {
+		return ignoreAction(action, prev) && ignoreAction(action, curr);
+	}, true);
+	return omitLogger && (window.__ENV__ !== `production`);
+}
+
+function ignoreAction(actionObj, actionType) {
+	return (actionObj.type !== actionType);
 }
